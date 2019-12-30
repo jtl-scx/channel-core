d6b87264b049d5336f94ce10afae94382a388859
avermeulen <andre.vermeulen@jtl-software.com>
2019-12-30 14:32:25 +0100

    EA-2621 add command to load global attributes

---
+++ src/MetaData/Attribute/ConditionalAttributeCollection.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+/**
+ * This File is part of JTL-Software
+ *
+ * User: mbrandt
+ * Date: 11/12/19
+ */
+
+namespace JTL\SCX\Lib\Channel\MetaData\Attribute;
+
+use JTL\Generic\GenericCollection;
+
+/**
+ * Class ConditionalAttributeCollection
+ * @package JTL\SCX\Lib\Channel\MetaData\Attribute
+ * @method ConditionalAttribute offsetGet($offset)
+ */
+class ConditionalAttributeCollection extends GenericCollection
+{
+    public function __construct()
+    {
+        parent::__construct(ConditionalAttribute::class);
+    }
+}
