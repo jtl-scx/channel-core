00ec2ca762cc92b4a4cfdc85bc0d5729f7cd2ff2
vermeulena <info@andrevermeulen.de>
2020-03-26 15:20:07 +0100

    Add helper to emit OrderShippedEvent (#4)

---
+++ src/Helper/Command/EmitOrderShippedEventCommand.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+/**
+ * This File is part of JTL-Software
+ *
+ * User: avermeulen
+ * Date: 2020-03-24
+ */
+
+namespace JTL\SCX\Lib\Channel\Helper\Command;
+
+use JTL\SCX\Client\Channel\Event\EventType;
+use Symfony\Component\Console\Input\InputOption;
+
+class EmitOrderShippedEventCommand extends AbstractEmitEventCommand
+{
+    protected static $defaultName = 'helper:emit.OrderShippingEvent';
+
+    protected function configure(): void
+    {
+        parent::configure();
+        $this->setDescription('Helper command to emit OrderShippedEvent for Testing')
+            ->addOption('orderId', null, InputOption::VALUE_REQUIRED, 'orderId', null)
+            ->addOption('shippingComplete', null, InputOption::VALUE_NONE, 'Is Shipping complete?', null)
+            ->addOption('shippedAt', null, InputOption::VALUE_REQUIRED, 'Shipping Date', null);
+    }
+
+    protected function getEventType(): EventType
+    {
+        return EventType::SellerOrderShipping();
+    }
+}
