51d5fcd0bb11c564a73c5bb570a6fd9a01b02fb1
vermeulena <info@andrevermeulen.de>
2020-04-07 09:04:55 +0200

    Add helper to emit OfferEnd Event (#5)

---
+++ src/Helper/Command/EmitOfferEndEventCommand.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+/**
+ * This File is part of JTL-Software
+ *
+ * User: avermeulen
+ * Date: 2020-03-24
+ */
+
+namespace JTL\SCX\Lib\Channel\Helper\Command;
+
+use JTL\SCX\Client\Channel\Event\EventType;
+use Symfony\Component\Console\Input\InputOption;
+
+class EmitOfferEndEventCommand extends AbstractEmitEventCommand
+{
+    protected static $defaultName = 'helper:emit.OfferEndEvent';
+
+    protected function configure(): void
+    {
+        parent::configure();
+        $this->setDescription('Helper command to emit OrderShippedEvent for Testing')
+            ->addOption('offerId', null, InputOption::VALUE_REQUIRED, 'offerId', null);
+    }
+
+    protected function getEventType(): EventType
+    {
+        return EventType::SellerOfferEnd();
+    }
+}
