84fb020b92b141ab6bc7d625617678b631b9eedc
avermeulen <andre.vermeulen@jtl-software.com>
2020-06-03 09:49:33 +0200

    EA-2820 add client

---
+++ src/Report/SellerInventory/Client/SendInventoryReportClient.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+/**
+ * This File is part of JTL-Software
+ *
+ * User: avermeulen
+ * Date: 2020-06-03
+ */
+
+namespace JTL\SCX\Lib\Channel\Report\SellerInventory\Client;
+
+use JTL\SCX\Client\Channel\Api\Report\ReportApi;
+use JTL\SCX\Client\Channel\Api\Report\Request\SendReportRequest;
+use JTL\SCX\Lib\Channel\Report\SellerInventory\Model\InventoryItemList;
+
+class SendInventoryReportClient
+{
+    private ReportApi $reportApi;
+
+    public function __construct(ReportApi $reportApi)
+    {
+        $this->reportApi = $reportApi;
+    }
+
+    /**
+     * @param string $sellerReportId
+     * @param InventoryItemList $itemList
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \JTL\SCX\Client\Exception\RequestFailedException
+     */
+    public function sendReport(string $sellerReportId, InventoryItemList $itemList)
+    {
+        $request = new SendReportRequest($sellerReportId, $itemList->toArray());
+        $this->reportApi->sendReport($request);
+    }
+}
