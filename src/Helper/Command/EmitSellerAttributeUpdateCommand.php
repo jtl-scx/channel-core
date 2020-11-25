b8e2f1f6672c2031199cc3898181aab326e744c1
Ronny H <ronny.herrgesell@jtl-software.com>
2020-11-25 15:29:28 +0100

    Add Helper Command to create SellerAttributeUpdate Request (#21)

---
+++ src/Helper/Command/EmitSellerAttributeUpdateCommand.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+/**
+ * This File is part of JTL-Software
+ *
+ * User: avermeulen
+ * Date: 2020-03-24
+ */
+
+namespace JTL\SCX\Lib\Channel\Helper\Command;
+
+use JTL\SCX\Client\Channel\Event\EventType;
+
+class EmitSellerAttributeUpdateCommand extends AbstractEmitEventCommand
+{
+    protected static $defaultName = 'helper:emit.SellerAttributeUpdateRequest';
+
+    protected function configure(): void
+    {
+        parent::configure();
+        $this->setDescription('Helper command to emit a SellerAttributeUpdate Request Event');
+    }
+
+    protected function getEventType(): EventType
+    {
+        return EventType::SellerMetaSellerAttributesUpdateRequest();
+    }
+}
