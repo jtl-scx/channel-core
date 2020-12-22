a27019f9d4778e3542bf87c665d8162fdb530bc3
Ronny Herrgesell <ronny.herrgesell@jtl-software.com>
2020-12-22 12:24:55 +0000

    re-assign $this to pass static analysis

--- src/Core/ToArrayTrait.php
+++ src/Core/ToArrayTrait.php
@@ -9,13 +9,20 @@
 
 namespace JTL\SCX\Lib\Channel\Core;
 
+use DateTimeImmutable;
+use Traversable;
+use function is_array;
+use function is_object;
+
 trait ToArrayTrait
 {
     public function toArray(): array
     {
-        if ($this instanceof \Traversable) {
+        if ($this instanceof Traversable) {
             $itemList = [];
-            foreach ($this as $item) {
+            /** @var Traversable $traversableObject */
+            $traversableObject = $this;
+            foreach ($traversableObject as $item) {
                 $itemList[] = $this->createArray($item);
             }
             return $itemList;
@@ -27,15 +34,15 @@ trait ToArrayTrait
     {
         $return = [];
         foreach ($data as $key => $attr) {
-            if (\is_object($attr)) {
-                if ($attr instanceof \DateTimeImmutable) {
+            if (is_object($attr)) {
+                if ($attr instanceof DateTimeImmutable) {
                     $return[$key] = $attr->format('c');
                 } elseif (method_exists($attr, 'toArray')) {
                     $return[$key] = $attr->toArray();
                 } else {
                     $return[$key] = $this->createArray((array)$attr);
                 }
-            } elseif (\is_array($attr)) {
+            } elseif (is_array($attr)) {
                 $return[$key] = $this->createArray($attr);
             } else {
                 $return[$key] = $attr;
