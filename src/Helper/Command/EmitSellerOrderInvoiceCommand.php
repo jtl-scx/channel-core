f3e30c2031bf78bc15ffd59b0627a71f133a572a
Ronny Herrgesell <ronny.herrgesell@jtl-software.com>
2021-01-08 11:19:20 +0000

    EA-3353: Helper Command to Emit SellerInvoice Event

---
+++ src/Helper/Command/EmitSellerOrderInvoiceCommand.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+/**
+ * This File is part of JTL-Software
+ *
+ * User: avermeulen
+ * Date: 2020-03-24
+ */
+
+namespace JTL\SCX\Lib\Channel\Helper\Command;
+
+use JTL\SCX\Client\Channel\Event\EventType;
+use Symfony\Component\Console\Input\InputOption;
+
+class EmitSellerOrderInvoiceCommand extends AbstractEmitEventCommand
+{
+    protected static $defaultName = 'helper:emit.OrderInvoice';
+
+    protected function configure(): void
+    {
+        parent::configure();
+        $this->setDescription('Helper command to emit a Order Invoice Event')
+            ->addOption('documentId', null, InputOption::VALUE_REQUIRED, 'documentId')
+            ->addOption('orderId', null, InputOption::VALUE_REQUIRED, 'orderId')
+            ->addOption('invoiceNumber', null, InputOption::VALUE_REQUIRED, 'invoiceNumber')
+            ->addOption('type', null, InputOption::VALUE_REQUIRED, 'type');
+    }
+
+    protected function getEventType(): EventType
+    {
+        return EventType::SellerEventOrderInvoice();
+    }
+}
