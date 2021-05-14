bf588aaa45e858a54b6733088b6d95844bb938e8
Ronny Herrgesell <ronny.herrgesell@jtl-software.com>
2021-05-14 19:38:07 +0000

    EA-3713: parse enum values correctly

--- src/MetaData/Attribute/AttributeEnumValueList.php
+++ src/MetaData/Attribute/AttributeEnumValueList.php
@@ -8,6 +8,7 @@
 
 namespace JTL\SCX\Lib\Channel\MetaData\Attribute;
 
+use InvalidArgumentException;
 use JTL\Generic\GenericCollection;
 use JTL\SCX\Lib\Channel\Core\ToArrayTrait;
 
@@ -23,7 +24,31 @@ class AttributeEnumValueList extends GenericCollection
         parent::__construct(AttributeEnumValue::class);
     }
 
-    public static function fromValueArray(?array $valueList): ?self
+    public static function fromArray(?array $array): self
+    {
+        $list = new self();
+        if ($array === null) {
+            return $list;
+        }
+
+        foreach ($array as $value) {
+            if (!isset($value['value'])) {
+                throw new InvalidArgumentException(
+                    'There must be at a array key "value"'
+                );
+            }
+
+            $display = null;
+            if (isset($value['display'])) {
+                $display = (string)$value['display'];
+            }
+
+            $list->add(new AttributeEnumValue((string)$value['value'], $display));
+        }
+        return $list;
+    }
+
+    public static function fromScalarArray(?array $valueList): ?self
     {
         if ($valueList === null) {
             return null;
@@ -32,7 +57,7 @@ class AttributeEnumValueList extends GenericCollection
         $list = new self();
         foreach ($valueList as $value) {
             if (!is_scalar($value)) {
-                throw new \InvalidArgumentException(
+                throw new InvalidArgumentException(
                     'AttributeEnumValueList can only be created from a array of scalar values'
                 );
             }
