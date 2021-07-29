8ede33b83110073e695a0472a748c314b590c4b8
avermeulen <andre.vermeulen@jtl-software.com>
2021-07-29 13:26:01 +0200

    EA-3868 Add Message/Listener to unlink Seller

---
+++ src/Seller/UnlinkSellerMessage.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace JTL\SCX\Lib\Channel\Seller;
+
+use JTL\Nachricht\Message\AbstractAmqpTransportableMessage;
+use JTL\SCX\Lib\Channel\Contract\Core\Message\SellerIdRelatedMessage;
+
+class UnlinkSellerMessage extends AbstractAmqpTransportableMessage implements SellerIdRelatedMessage
+{
+    private ChannelSellerId $channelSellerId;
+    private ?string $reason;
+
+    public function __construct(ChannelSellerId $channelSellerId, string $reason = null, string $messageId = null)
+    {
+        parent::__construct($messageId);
+        $this->channelSellerId = $channelSellerId;
+        $this->reason = $reason;
+    }
+
+    public function getSellerId(): ChannelSellerId
+    {
+        return $this->channelSellerId;
+    }
+
+    public function getReason(): ?string
+    {
+        return $this->reason;
+    }
+}
