87c9b988873e4ce83ee6c76f74a1290aeffeedaf
Ronny Herrgesell <ronny.herrgesell@jtl-software.com>
2021-09-29 08:37:33 +0000

    EA-4027: Implement Sleeper using time_nanosleep

---
+++ src/Core/PhpNanoSleep.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace JTL\SCX\Lib\Channel\Core;
+
+use JTL\SCX\Lib\Channel\Contract\Core\Sleeper;
+
+final class PhpNanoSleep implements Sleeper
+{
+    public function sleep(float $seconds): void
+    {
+        $fullSec = (int)floor($seconds);
+        $decimalPlaces = $seconds - $fullSec;
+        $nano = (int)($decimalPlaces * 1000000);
+        time_nanosleep($fullSec, $nano);
+    }
+}
--- tests/Core/PhpNanoSleepTest.php
+++ tests/Core/PhpNanoSleepTest.php
@@ -5,22 +5,22 @@ namespace JTL\SCX\Lib\Channel\Core;
 use PHPUnit\Framework\TestCase;
 
 /**
- * @covers \JTL\SCX\Lib\Channel\Core\PhpUsleep
+ * @covers \JTL\SCX\Lib\Channel\Core\PhpNanoSleep
  */
-class PhpUsleepTest extends TestCase
+class PhpNanoSleepTest extends TestCase
 {
     /**
      * @test
      */
     public function it_can_sleep(): void
     {
-        $sut = new PhpUsleep();
+        $sut = new PhpNanoSleep();
 
-        $wait = 0.05;
+        $wait = 0.13089;
         $start = microtime(true);
         $sut->sleep($wait);
         $now = microtime(true);
 
-        self::assertGreaterThanOrEqual($start + $wait, $now);
+        self::assertGreaterThanOrEqual($now, $start + $wait);
     }
 }
