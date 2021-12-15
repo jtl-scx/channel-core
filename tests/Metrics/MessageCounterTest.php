6f43f6b88e27a536f8daf3ec3d1326a93ce1c21e
Patric Kanngiesser <patric.kanngiesser@jtl-software.com>
2021-12-15 17:02:43 +0100

    EA-4272: Add Tests

---
+++ tests/Metrics/MessageCounterTest.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace Core\Metrics;
+
+use JTL\Nachricht\Contract\Message\Message;
+use JTL\SCX\Lib\Channel\Core\Metrics\Counter;
+use JTL\SCX\Lib\Channel\Core\Metrics\MessageCounter;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class MessageCounterTest
+ * @package Core\Metrics
+ *
+ * @covers \JTL\SCX\Lib\Channel\Core\Metrics\MessageCounter
+ */
+class MessageCounterTest extends TestCase
+{
+    public function testCanCountMessage()
+    {
+        $counter = $this->createMock(Counter::class);
+        $messageCounter = new MessageCounter($counter);
+
+        $message = $this->createMock(Message::class);
+        $counter->expects($this->once())
+            ->method('countKey');
+
+        $messageCounter->countMessage($message);
+    }
+}
