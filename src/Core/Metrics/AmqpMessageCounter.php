f338c6d8fcc9e9f62342e30b07dee4b1768ace78
Marius Brandt <Marius.Brandt@jtl-software.com>
2023-03-15 11:09:51 +0100

    EA-5333: implement AmqpMetrics

---
+++ src/Core/Metrics/AmqpMessageCounter.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JTL\SCX\Lib\Channel\Core\Metrics;
+
+use GuzzleHttp\Exception\GuzzleException;
+use JTL\GoPrometrics\Client\Counter;
+use JTL\GoPrometrics\Client\Label;
+use JTL\GoPrometrics\Client\LabelList;
+use JTL\Nachricht\Contract\Message\Message;
+use JTL\Nachricht\Contract\Message\MessageCounter;
+use JTL\OpsGenie\Client\Heartbeat\PingRequest;
+use JTL\OpsGenie\Client\HeartbeatApiClient;
+use JTL\SCX\Lib\Channel\Core\Environment\Environment;
+use Psr\Log\LoggerInterface;
+
+class AmqpMessageCounter implements MessageCounter
+{
+    public function __construct(
+        private readonly Counter $counter,
+        private readonly LoggerInterface $logger,
+        private readonly HeartbeatApiClient $heartbeatApiClient,
+        private readonly Environment $environment
+    ) {
+    }
+
+    public function countMessage(Message $message): void
+    {
+        $labelList = new LabelList();
+        $labelList->add(new Label('message', get_class($message)));
+        $channelName = $this->environment->get('CHANNEL_NAME');
+
+        $this->heartbeatApiClient->sendPing(new PingRequest("SCX_{$channelName}_worker_heartbeat"));
+
+        try {
+            $this->counter->count('EA', 'messages_total', $labelList);
+        } catch (GuzzleException $e) {
+            $this->logger->error("Failed to collect metric 'messages_total': {$e->getMessage()}");
+        }
+    }
+}
---
+++ tests/Core/Metrics/AmqpMessageCounterTest.php
@@ -0,0 +1,70 @@
+<?php declare(strict_types=1);
+/**
+ * This File is part of JTL-Software
+ *
+ * User: marius
+ * Date: 3/15/23
+ */
+
+namespace JTL\SCX\Lib\Channel\Core\Metrics;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+use JTL\GoPrometrics\Client\Counter;
+use JTL\Nachricht\Contract\Message\Message;
+use JTL\OpsGenie\Client\HeartbeatApiClient;
+use JTL\OpsGenie\Client\HttpClient;
+use JTL\SCX\Lib\Channel\Core\Environment\Environment;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class AmqpMessageCounter
+ *
+ * @package JTL\SCX\Lib\Channel\Core\Metrics
+ *
+ * @covers \JTL\SCX\Lib\Channel\Core\Metrics\AmqpMessageCounter
+ */
+class AmqpMessageCounterTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function canCountMessage(): void
+    {
+        $message = new AmqpTestMessage();
+        $channelName = uniqid('channelName', true);
+
+        $gpCounter = $this->createMock(Counter::class);
+        $logger = $this->createMock(LoggerInterface::class);
+        $guzzle = $this->createMock(Client::class);
+        $httpClient = new HttpClient('abc', $guzzle);
+        $heartbeat = new HeartbeatApiClient($httpClient);
+        $environment = $this->createMock(Environment::class);
+        $counter = new AmqpMessageCounter($gpCounter, $logger, $heartbeat, $environment);
+        $heartbeatResponse = new Response(body: '{}');
+
+        $environment->expects(self::once())
+            ->method('get')
+            ->with('CHANNEL_NAME')
+            ->willReturn($channelName);
+
+        $guzzle->expects(self::once())
+            ->method('request')
+            ->with('PUT', "heartbeats/SCX_{$channelName}_worker_heartbeat/ping", [
+                'headers' => [
+                    'Authorization' => 'GenieKey abc',
+                    'Content-Type' => 'application/json',
+                ],
+                'body' => '{}',
+            ])
+            ->willReturn($heartbeatResponse);
+
+        $counter->countMessage($message);
+    }
+}
+
+class AmqpTestMessage implements Message
+{
+
+}
