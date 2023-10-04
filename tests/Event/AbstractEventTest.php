57411082201a90d673d99b20a79eb4ee7fc91ee0
Ronny Herrgesell <ronny.herrgesell@jtl-software.com>
2023-10-04 15:31:02 +0200

    EA-5968: Add delay and retry handling to Abstract Event
    * switch default retryDelay to 120

--- tests/Event/AbstractEventTest.php
+++ tests/Event/AbstractEventTest.php
@@ -10,22 +10,25 @@ use PHPUnit\Framework\TestCase;
  */
 class AbstractEventTest extends TestCase
 {
-    public function testCanBeUsed(): void
+    public function test_it_can_be_constructed_correctly(): void
     {
-        $id = uniqid();
-        $createdAt = new \DateTimeImmutable();
-        $type = $this->createStub(EventType::class);
-        $internalEventId = uniqid();
-
-        $sut = new TestEvent($id, $createdAt, $type, $internalEventId);
+        $sut = new class (
+            id: $id = uniqid(),
+            createdAt:  $createdAt = new \DateTimeImmutable(),
+            type: $type = $this->createStub(EventType::class),
+            internalEventId: $internalEventId = uniqid(),
+            delay: $delay = random_int(1, 10000),
+            retryDelay: $retryDelay = random_int(1, 10000),
+            retryCount: $retryCount = random_int(1, 10000)
+        ) extends AbstractEvent {
+        };
 
         self::assertSame($id, $sut->getId());
         self::assertSame($createdAt, $sut->getCreatedAt());
         self::assertSame($type, $sut->getType());
         self::assertSame($internalEventId, $sut->getMessageId());
+        self::assertEquals($delay, $sut->getDelay());
+        self::assertEquals($retryDelay, $sut->getRetryDelay());
+        self::assertEquals($retryCount, $sut->getRetryCount());
     }
-}
-
-class TestEvent extends AbstractEvent
-{
 }
