7ef6eb2eff5bbf1984f313164179ebc8d49cc261
Ronny Herrgesell <ronny.herrgesell@jtl-software.com>
2024-03-20 14:37:16 +0100

    EA-6284: Add boolean value retrieval to AttributeSelector
    This update adds a method to the AttributeSelector class that allows for the retrieval of boolean attribute values from a list of channel attributes.

--- src/Client/Attribute/AttributeSelector.php
+++ src/Client/Attribute/AttributeSelector.php
@@ -12,6 +12,11 @@ namespace JTL\SCX\Lib\Channel\Client\Attribute;
 
 use JTL\SCX\Lib\Channel\Client\Model\ChannelAttribute;
 
+/**
+ * Class AttributeSelector
+ *
+ * This class provides methods to select and retrieve attribute values from a given list of channel attributes.
+ */
 class AttributeSelector
 {
     /**
@@ -22,7 +27,6 @@ class AttributeSelector
     public function getValueListById(string $attributeId, array $channelAttributeList): ?array
     {
         $results = [];
-        /** @var ChannelAttribute $attribute */
         foreach ($channelAttributeList as $attribute) {
             if ($attribute->getAttributeId() === $attributeId) {
                 $results[] = $attribute->getValue();
@@ -33,17 +37,38 @@ class AttributeSelector
 
     /**
      * @param string $attributeId
-     * @param array $channelAttributeList
+     * @param ChannelAttribute[] $channelAttributeList
      * @return string|null
      */
     public function getValueById(string $attributeId, array $channelAttributeList): ?string
     {
-        /** @var ChannelAttribute $attribute */
         foreach ($channelAttributeList as $attribute) {
             if ($attribute->getAttributeId() === $attributeId) {
                 return $attribute->getValue();
             }
         }
         return null;
+    }
+
+
+    /**
+     * Retrieves a boolean value by attribute name from a list of channel attributes.
+     *
+     * @param string $attributeId The attribute name to search for.
+     * @param ChannelAttribute[] $channelAttributeList The list of channel attributes.
+     * @param bool|null $default (optional) The default value to return if the attribute is not found. Defaults to null.
+     * @return bool|null The boolean value corresponding to the attribute name, or the default value if not found.
+     */
+    public function getBooleanById(string $attributeId, array $channelAttributeList, bool|null $default = null): bool|null
+    {
+        $value = $this->getValueById($attributeId, $channelAttributeList);
+        if ($value === null) {
+            return $default;
+        }
+
+        return match(strtolower($value)) {
+            'true', '1', 'yes', 'y', 'ja', 'j', 'on', 'w', 'wahr' => true,
+            default => false,
+        };
     }
 }
--- tests/Client/Attribute/AttributeSelectorTest.php
+++ tests/Client/Attribute/AttributeSelectorTest.php
@@ -18,7 +18,7 @@ use PHPUnit\Framework\TestCase;
  */
 class AttributeSelectorTest extends TestCase
 {
-    public function testGetValueListById()
+    public function testGetValueListById(): void
     {
         $testId = uniqid('testid');
         $list = [
@@ -36,7 +36,7 @@ class AttributeSelectorTest extends TestCase
         $this->assertEquals('baz', $result[1]);
     }
 
-    public function testGetNullWhenAttributeNotExistsInList()
+    public function testGetNullWhenAttributeNotExistsInList(): void
     {
         $list = [
             new ChannelAttribute(['attributeId' => 'foo', 'value' => 'bar']),
@@ -48,7 +48,7 @@ class AttributeSelectorTest extends TestCase
         $this->assertNull($result);
     }
 
-    public function testCanGetOneSingleValueById()
+    public function testCanGetOneSingleValueById(): void
     {
         $testId = uniqid('testid');
         $list = [
@@ -62,7 +62,7 @@ class AttributeSelectorTest extends TestCase
         $this->assertEquals('bar', $result);
     }
 
-    public function testGetNullWhenAttributeNotExists()
+    public function testGetNullWhenAttributeNotExists(): void
     {
         $list = [
             new ChannelAttribute(['attributeId' => 'foo', 'value' => 'bar']),
@@ -74,7 +74,7 @@ class AttributeSelectorTest extends TestCase
         $this->assertNull($result);
     }
 
-    public function testFirstAttributeIdIsSelected()
+    public function testFirstAttributeIdIsSelected(): void
     {
         $list = [
             new ChannelAttribute(['attributeId' => 'foo', 'value' => 'first']),
@@ -88,4 +88,46 @@ class AttributeSelectorTest extends TestCase
         $this->assertEquals('first', $result);
         $this->assertNotEquals('second', $result);
     }
+
+    /**
+     * @test
+     */
+    public function it_can_fetch_a_boolean_value_from_AttributeList(): void
+    {
+        $list = [
+            new ChannelAttribute(['attributeId' => '1', 'value' => 'true']),
+            new ChannelAttribute(['attributeId' => '2', 'value' => 'false']),
+            new ChannelAttribute(['attributeId' => '3', 'value' => 'yes']),
+            new ChannelAttribute(['attributeId' => '4', 'value' => 'no']),
+            new ChannelAttribute(['attributeId' => '5', 'value' => '1']),
+            new ChannelAttribute(['attributeId' => '6', 'value' => '0']),
+            new ChannelAttribute(['attributeId' => '7', 'value' => 'ja']),
+            new ChannelAttribute(['attributeId' => '8', 'value' => 'nein']),
+            new ChannelAttribute(['attributeId' => '9', 'value' => 'on']),
+            new ChannelAttribute(['attributeId' => '10', 'value' => 'off']),
+            new ChannelAttribute(['attributeId' => '100', 'value' => 'Natürlich']),
+        ];
+        $sut = new AttributeSelector();
+
+        self::assertNull($sut->getBooleanById('not-in-list', $list, null));
+        self::assertFalse($sut->getBooleanById('not-in-list', $list, false));
+        self::assertTrue($sut->getBooleanById('not-in-list', $list, true));
+
+        self::assertTrue($sut->getBooleanById('1', $list));
+        self::assertTrue($sut->getBooleanById('3', $list));
+        self::assertTrue($sut->getBooleanById('5', $list));
+        self::assertTrue($sut->getBooleanById('7', $list));
+        self::assertTrue($sut->getBooleanById('9', $list));
+
+        self::assertFalse($sut->getBooleanById('2', $list));
+        self::assertFalse($sut->getBooleanById('4', $list));
+        self::assertFalse($sut->getBooleanById('6', $list));
+        self::assertFalse($sut->getBooleanById('8', $list));
+        self::assertFalse($sut->getBooleanById('10', $list));
+
+        self::assertNull($sut->getBooleanById('Bier', $list));
+        self::assertFalse($sut->getBooleanById('100', $list));
+    }
+
+
 }
