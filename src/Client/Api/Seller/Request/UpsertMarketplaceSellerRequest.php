a98b8e31bf34d461e7c09fb983726e757f785611
Kai-Michael Schwendig <kai.schwendig@jtl-software.com>
2026-01-26 09:40:08 +0100

    EA-7420 ✅ Add UpsertMarketplaceSellerRequest and UpsertMarketplaceSellerResponse classes

---
+++ src/Client/Api/Seller/Request/UpsertMarketplaceSellerRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JTL\SCX\Lib\Channel\Client\Api\Seller\Request;
+
+use JTL\SCX\Client\Request\ScxApiRequest;
+use JTL\SCX\Lib\Channel\Client\Api\AbstractScxApiRequest;
+use JTL\SCX\Lib\Channel\Client\Model\UpsertMarketplaceSeller;
+
+class UpsertMarketplaceSellerRequest extends AbstractScxApiRequest
+{
+
+    public function __construct(
+        private readonly string $jtlAccountId,
+        private readonly UpsertMarketplaceSeller $upsertMarketplaceSeller)
+    {}
+
+    public static function make(string $jtlAccountId, string $sellerId, bool $isActive = true, string $companyName = '' ): UpsertMarketplaceSellerRequest
+    {
+        return new self($jtlAccountId, new UpsertMarketplaceSeller([
+            'sellerId' => $sellerId,
+            'isActive' => $isActive,
+            'companyName' => $companyName,
+        ]));
+    }
+
+    public function getUrl(): string
+    {
+        return '/v1/channel/seller/{jtlAccountId}';
+    }
+
+    public function getHttpMethod(): string
+    {
+        return ScxApiRequest::HTTP_METHOD_POST;
+    }
+
+    public function getUpsertMarketplaceSellerModel(): UpsertMarketplaceSeller
+    {
+        return $this->upsertMarketplaceSeller;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getParams(): array
+    {
+        return [
+            'jtlAccountId' => $this->jtlAccountId,
+        ];
+    }
+
+    public function getBody(): string
+    {
+        return (string)$this->getUpsertMarketplaceSellerModel();
+    }
+}
